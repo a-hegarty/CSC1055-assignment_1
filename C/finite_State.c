a59799a501e101856d353c9ca5027a45a0e5cf19
A.Hegarty <a.hegarty004@gmail.com>
2025-10-25 17:27:44 +0100

    added finite_State toC folder

---
+++ C/finite_State.c
@@ -0,0 +1,10 @@
+//code written by Aoibhe Hegarty
+//student number: 23487084
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Finite State Machine Implementation\n");
+    // Your finite state machine code goes here
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
 # CSC1055-assignment_1
+
 23487084 - aoibhe hegarty
 23431826 - toby savino
